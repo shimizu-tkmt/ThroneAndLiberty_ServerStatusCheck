f35aa3733824fa3f2d6bd1e7c334cf3398f282ba
shimizu-tkmt <shimizu.tkmt@gmail.com>
2025-02-05 09:42:30 +0900

    if __name__ == '__main__':　を追加。 ReadMe.md編集

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # ThroneAndLiberty_ServerStatusCheck
 # 概要
-## THTONE AND LIVERTY　の　ALEXIAサーバーのステータスをチェックするアプリケーションです。
+- THTONE AND LIVERTYのALEXIAサーバーのステータスをチェックするアプリケーションです。
 
 ## 下記のWEBサイトをチェックしています。
-
 https://www.playthroneandliberty.com/ja-jp/support/server-status
 
 # 動作内容
-## 実行するとWindowsコンソールが立ち上がり20秒に一回サイトをチェックします。
-## メンテナンス状態でない場合は、BEEP音3回鳴らしてMsgBox表示して終了します。
+- 実行するとWindowsコンソールが立ち上がり20秒に一回サイトをチェックします。
+- メンテナンス状態でない場合は、BEEP音3回鳴らしてMsgBox表示して終了します。
+
+# exe作成方法
+1. pyinstallerをインストール
+~~~ windows shell
+pip install pyinstaller
+~~~ 
+
+2. コマンドプロンプトで作業フォルダに移動し下記を実行
+~~~ windows shell
+pyinstaller ThroneAndLiberty_ServerStatusCheck.py
+~~~
--- ThroneAndLiberty_ServerStatusCheck.py
+++ ThroneAndLiberty_ServerStatusCheck.py
@@ -11,44 +11,48 @@ sleep_time = 20
 msg_title = 'ThroneAndLiberty_ServerStatusCheck'
 item = ''
 
-try:
-    while True:
-        # HTMLの取得(GET)
-        req = requests.get(url, timeout=6.0)
-        req.encoding = req.apparent_encoding # 日本語の文字化け防止
+def main():
+    try:
+        while True:
+            # HTMLの取得(GET)
+            req = requests.get(url, timeout=6.0)
+            req.encoding = req.apparent_encoding # 日本語の文字化け防止
 
-        # HTMLの解析
-        bsObj = BeautifulSoup(req.text,"html.parser")
+            # HTMLの解析
+            bsObj = BeautifulSoup(req.text,"html.parser")
 
-        # 要素の抽出
-        spans = bsObj.find_all("span")
-        for span in spans:
-            #print(span.get_text())
-            if span.get_text() == server_name:
-                item = span.get("aria-label")
-                break
+            # 要素の抽出
+            spans = bsObj.find_all("span")
+            for span in spans:
+                #print(span.get_text())
+                if span.get_text() == server_name:
+                    item = span.get("aria-label")
+                    break
 
-        #現在日時出力
-        dt_now = datetime.datetime.now()
-        print(dt_now)
+            #現在日時出力
+            dt_now = datetime.datetime.now()
+            print(dt_now)
 
-        print(item)
-        #　右から１１文字で判別
-        if item[-11:] != 'Maintenance':  # 取り出した要素が偽と見なされる値（空文字列など）であれば 
-            break  # 無限ループを終了
-        time.sleep(sleep_time)  # 20秒間スリープ
+            print(item)
+            #　右から１１文字で判別
+            if item[-11:] != 'Maintenance':  # 取り出した要素が偽と見なされる値（空文字列など）であれば 
+                break  # 無限ループを終了
+            time.sleep(sleep_time)  # 20秒間スリープ
 
-    # ビープ音を鳴らす　３回音程変えて鳴らす
-    #winsound.Beep(frequency, duration)
-    winsound.Beep(2500, 500)
-    winsound.Beep(3000, 500)
-    winsound.Beep(4000, 500)
+        # ビープ音を鳴らす　３回音程変えて鳴らす
+        #winsound.Beep(frequency, duration)
+        winsound.Beep(2500, 500)
+        winsound.Beep(3000, 500)
+        winsound.Beep(4000, 500)
 
-    # MsgBox表示
-    user32 = windll.user32
-    user32.MessageBoxW(0, item,msg_title, 0x00000000|0x00000040)
+        # MsgBox表示
+        user32 = windll.user32
+        user32.MessageBoxW(0, item,msg_title, 0x00000000|0x00000040)
 
-except Exception as e:
-    print(f"An error occurred: {e}")
-    input("Press Enter to close the program...")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        input("Press Enter to close the program...")
 
+#モジュール呼出時の実行防止
+if __name__ == '__main__':
+    main()
